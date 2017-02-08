219fa7237b44c4cc52cf47e706c2ad2e2499b64d
whatif85 <geeckoguy@gmail.com>
2017-02-08 14:45:44 -0500

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		// the string names come from Edit>Project Settings>Input Manager for key binding
+		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		Debug.Log ("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
